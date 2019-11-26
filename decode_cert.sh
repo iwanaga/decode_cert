673a4fb30a249586b36130bc31c6209200137c04
Yoshihiro <iwanaga.blackie@gmail.com>
2019-11-26 14:36:09 +0900

    check 1 argument is specified

--- decode_cert.sh
+++ decode_cert.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+function die () {
+  echo >&2 "$@"
+  exit 1
+}
+
+[[ $# -eq 1 ]] || die 'a certificate file required.'
+
 openssl x509 -text -noout -in $1 | grep -e Subject: -e Issuer: | sort --reverse
